d402c103c76adaaa4f67905b029149585d0b25c0
nmerris <nate.merris@gmail.com>
2017-07-27 17:13:44 -0400

    Implement basic structure in main

--- src/mypackage/InvoiceApp.java
+++ src/mypackage/InvoiceApp.java
@@ -1,9 +1,36 @@
 package mypackage;
 
+import java.util.Scanner;
+
 public class InvoiceApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		double taxRate; // one tax rate for all products
+		boolean addMoreProducts = true; // set to false if user chooses to stop entering products
+		
+		
+		System.out.println("Enter the tax rate: ");
+		taxRate = scanner.nextDouble(); // store tax rate
+		scanner.nextLine(); // consume the dangling \n
+		
+		// continue asking user for more products until they say stop
+		do {
+			// TODO: get 'er done
+		
+		
+		
+		
+		
+		
+			System.out.println("Would you like to add another product? Y/N");
+			addMoreProducts = scanner.nextLine().equalsIgnoreCase("y") ? true : false;
+		} while(addMoreProducts);
+		
+		
+		
+		
 
 	}
 
