7cc7ac6551304b3c53f6eb5017e1cc26ae58a837
nmerris <nate.merris@gmail.com>
2017-07-27 17:02:38 -0400

    Add getters, setters to Product class, also add comments

--- src/mypackage/Product.java
+++ src/mypackage/Product.java
@@ -1,5 +1,48 @@
 package mypackage;
 
 public class Product {
+	
+	private double price;
+	private String description;
+	
+	
+	/**
+	 * Creates a new product
+	 * @param price the product price
+	 * @param description the product description
+	 */
+	public Product(double price, String description) {
+		
+	}
+
+	/**
+	 * @return the product price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the product price
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	/**
+	 * @return the product description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the product description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
 
 }
